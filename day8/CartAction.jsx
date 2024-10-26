8dd70cf55316452d8d9cdfa301005272693f4e6d
saburnisa <150768874+saburnisa@users.noreply.github.com>
2024-10-26 13:52:36 +0530

    Add files via upload

---
+++ day8/CartAction.jsx
@@ -0,0 +1,16 @@
+
+export const addItem = (item) => ({
+    type: 'ADD_ITEM',
+    payload: item,
+  });
+  
+  export const removeItem = (id) => ({
+    type: 'REMOVE_ITEM',
+    payload: { id },
+  });
+  
+  export const updateItem = (item) => ({
+    type: 'UPDATE_ITEM',
+    payload: item,
+  });
+  
---
+++ day8/store.jsx
@@ -0,0 +1,7 @@
+
+import { createStore } from 'redux';
+import { cartReducer } from './CartReducer';
+
+const store = createStore(cartReducer);
+
+export default store;
